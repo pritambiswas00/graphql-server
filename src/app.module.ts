efd651b0d2cfbe6eaec527e4e041bb9101ea4ab5
pritam biswas <pritamb@creditnirvana.ai>
2023-06-07 16:06:01 +0530

    changes the path directory.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { MoviesModule } from './movies/movies.module';
+import { AppController } from '~/app.controller';
+import { AppService } from '~/app.service';
+import { AuthModule } from '~/auth/auth.module';
+import { UserModule } from '~/user/user.module';
+import { MoviesModule } from '~/movies/movies.module';
 
 @Module({
   imports: [AuthModule, UserModule, MoviesModule],
--- src/movies/movies.controller.spec.ts
+++ src/movies/movies.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { MoviesController } from './movies.controller';
+import { MoviesController } from '~/movies/movies.controller';
 
 describe('MoviesController', () => {
   let controller: MoviesController;
