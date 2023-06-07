584829275f9abf58d877a4e4db7f189d6b3c5077
pritam biswas <pritamb@creditnirvana.ai>
2023-06-07 14:27:28 +0530

    added module and controller.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, MoviesModule],
   controllers: [AppController],
   providers: [AppService],
 })
